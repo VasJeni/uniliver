a5102fa779a2f0bc8df2c52c14027345ba61b630
VasJeni <vasilyev508@gmail.com>
2018-08-21 17:39:10 +0300

    little add adaptive

--- js/main.js
+++ js/main.js
@@ -1,18 +1,35 @@
 function openResumeForm() {
-    let formVisibility = document.getElementsByClassName("resume-form");
+    let formVisibility = document.getElementsByClassName("form-wrap");
     formVisibility[0].className += " d-block";
 }
-
-function closeResumeForm() {
-    let getForm = document.getElementsByClassName("resume-form");
-    getForm[0].classList.remove("d-block");
+function f() {
     return true;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    const $btnOpenForm = document.querySelector('.js-btn-open-resume-form');
-    const $btnCloseForm = document.querySelector('.js-close-resume-form');
+function closeResumeForm() {
+    let getForm = document.getElementsByClassName("form-wrap");
+    getForm[0].classList.remove("d-block");
+    console.log('123');
+}
 
-    $btnOpenForm.addEventListener('click', openResumeForm);
+document.addEventListener('DOMContentLoaded', function () {
+    let $btnOpenForm = document.querySelectorAll('.js-btn-open-resume-form');
+    const $btnCloseForm = document.querySelector('.js-close-resume-form');
+    const $closeFormByWrap = document.querySelector('.js-close-resume-form');
     $btnCloseForm.addEventListener('click', closeResumeForm);
+
+    $('.js-form-area').on('click', function(e) {
+        e.stopPropagation();
+    });
+    let getTel = document.getElementById('tel');
+    let maskOptions = {
+        mask : '+(3) (000) 000-00-00'
+    };
+    let mask = new IMask(getTel, maskOptions);
+
+    const $closeForm = document.querySelector('.js-close-form-by-wrap');
+    $closeForm.addEventListener('click',closeResumeForm );
+    for (let i=0; i<$btnOpenForm.length; i++){
+        $btnOpenForm[i].addEventListener('click', openResumeForm);
+    }
 });
