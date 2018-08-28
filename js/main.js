f1f7e675c16ac425eb660a29f18d940ce6723bf2
JasJeni <Vasilyev508@gmail.com>
2018-08-28 18:19:48 +0300

    fix second video todo fix first video

--- js/main.js
+++ js/main.js
@@ -1,50 +1,62 @@
-function openResumeForm() {
-    let formVisibility = document.getElementsByClassName("form-wrap");
-    formVisibility[0].className += " d-block";
-    console.log('openResumeForm');
-}
-let firsVideoSrc = 'https://www.youtube.com/embed/ZRjE6u63iJ0';
-let secondVideoSrc = 'https://www.youtube.com/embed/dbWYqdBNgH4';
 
-function closeResumeForm() {
-    let getForm = document.getElementsByClassName("js-close-form-by-wrap");
-    getForm[0].classList.remove("d-block");
-    console.log('closeResumeForm');
-}
-
-function openFirstVideo() {
-    let videoWrap = document.getElementsByClassName('first-video');
-    videoWrap[0].className += ' d-block';
-    console.log('openFirstVideo');
-}
-function openSecondVideo() {
-    let videoWrap = document.getElementsByClassName('second-video');
-    let videoArea = document.getElementsByClassName('js-video-wrap');
-    videoWrap[0].className += ' d-block';
-    console.log('openSecondVideo');
-}
-function closeVideoByWrap(){
-    let getVideoWrap = document.getElementsByClassName('video');
-    player.pauseVideo();
-    getVideoWrap[0].classList.remove('d-block');
-    console.log('closeVideoByWrap');
-
-}
 
 
 document.addEventListener('DOMContentLoaded', function () {
+    //начало сдвинутого куска
+
+    function openResumeForm() {
+        let formVisibility = document.getElementsByClassName("form-wrap");
+        formVisibility[0].className += " d-block";
+        console.log('openResumeForm');
+    }
+    let firsVideoSrc = 'https://www.youtube.com/embed/ZRjE6u63iJ0';
+    let secondVideoSrc = 'https://www.youtube.com/embed/dbWYqdBNgH4';
+
+    function closeResumeForm() {
+        let getForm = document.getElementsByClassName("form-wrap");
+        getForm[0].classList.remove(" d-block");
+    }
+
+    function openFirstVideo() {
+        let videoWrap = document.getElementsByClassName('video');
+        /*   let videoArea = document.getElementsByClassName('js-video-wrap');*/
+        /*    videoArea[0].setAttribute('src',firsVideoSrc);*/
+        videoWrap[0].className += ' d-block';
+        console.log('openFirstVideo');
+    }
+    function openSecondVideo() {
+        let videoSecondWrap = document.getElementsByClassName('second-video');
+        // let videoArea = document.getElementsByClassName('js-video-wrap');
+        //videoArea[0].setAttribute('src',secondVideoSrc);
+        videoSecondWrap[0].className += ' d-block';
+        console.log('openSecondVideo');
+    }
+    function closeFirstVideoByWrap(){
+        let getVideoWrap = document.getElementsByClassName('video');
+        getVideoWrap[0].classList.remove('d-block');
+        player.pauseVideo();
+        console.log('closeVideoByWrap');
+    }
+    function closeSecondVideo(){
+        let getSecondVideoWrap = document.getElementsByClassName('second-video');
+        getSecondVideoWrap[0].classList.remove('d-block');
+        player2.pauseVideo();
+        console.log('closeSecondVideo');
+    }
+    //вставленый перемещённый кусок
+
+    // 2. This code loads the IFrame Player API code asynchronously.
+    var tag = document.createElement('script');
+    tag.src = "http://www.youtube.com/player_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
     let $btnOpenForm = document.querySelectorAll('.js-btn-open-resume-form');
     const $firstVideo = document.querySelector('.capabilities__item__img-f');
     const $btnCloseForm = document.querySelector('.js-close-resume-form');
     const $FormWrap = document.querySelector('.js-close-resume-form');
     const $secondVideo = document.querySelector('.capabilities__item__img-s');
-    const $closeForm = document.querySelector('.js-close-form-by-wrap');
-    const $closeVideo = document.querySelector('.video');
-
-
-
     $btnCloseForm.addEventListener('click', closeResumeForm);
-    $closeForm.addEventListener('click',closeResumeForm );
+
     $('.js-form-area').on('click', function(e) {
         e.stopPropagation();
     });
@@ -52,36 +64,27 @@ document.addEventListener('DOMContentLoaded', function () {
         a.stopPropagation();
     });
 
-
-
     let getTel = document.getElementById('tel');
     let maskOptions = {
         mask : '+(3) (000) 000-00-00'
     };
     let mask = new IMask(getTel, maskOptions);
 
+    const $closeForm = document.querySelector('.js-close-form-by-wrap');
+    const $closeVideo = document.querySelector('.video');
+    const $closeSecondVideo = document.querySelector('.second-video');
 
-
-
+    $closeForm.addEventListener('click',closeResumeForm );
 
     for (let i=0; i<$btnOpenForm.length; i++){
         $btnOpenForm[i].addEventListener('click', openResumeForm);
     }
 
-    $firstVideo.addEventListener('click', openFirstVideo);
+    $firstVideo.addEventListener('click',  openFirstVideo);
     $secondVideo.addEventListener('click', openSecondVideo);
 
-    $closeVideo.addEventListener('click', closeVideoByWrap);
-    var player;
-    function onYouTubePlayerAPIReady() {
-        player = new YT.Player('player', {
-        });
-        document.getElementById('resume').onclick = function() {
-            player.playVideo();
-        };
-        document.getElementById('close_vid').onclick = function() {
-            player.pauseVideo();
-        };
-    }
+    $closeVideo.addEventListener('click', closeFirstVideoByWrap);
+    $closeSecondVideo.addEventListener('click', closeSecondVideo);
+
 
 });
